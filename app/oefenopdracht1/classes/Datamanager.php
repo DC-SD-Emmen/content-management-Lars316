3edbd3a15619496361feccc598374a7c8b75dc44
Lars316 <144122@student.dcterra.nl>
2025-02-13 12:59:30 +0100

    Re-re-re-regex
    Een goede regex functie maken voor een password is moeilijker dan het lijkt, teminste dat vind ik.

--- app/oefenopdracht1/classes/Datamanager.php
+++ app/oefenopdracht1/classes/Datamanager.php
@@ -2,169 +2,107 @@
 <?php
 
 class DataManager {
-    private $servername = "mysql";
-    private $username = "root";
-    private $password = "root";
-    private $dbname = "user_login";
-    private $conn;
+  private $servername = "mysql";
+  private $username = "root";
+  private $password = "root";
+  private $dbname = "user_login";
+  private $conn;
 
-    public function __construct(Database $db) {
-        $this->conn = $db->getConnection();
+  public function __construct(Database $db) {
+      $this->conn = $db->getConnection();
+  }
+
+  public function insert($data, $fileName) {
+
+      $username = htmlspecialchars($data['username']); 
+      $password = htmlspecialchars($data['password']);
+
+      $generalRegex = '/(?!<>\/;\\[\\]{}`~)[A-Za-z0-9 ]*/';
+      $usernameRegex = '/[A-Za-z0-9_]*/';
+
+      // $passwordRegex = '/placeholder/';
+  
+      if (!preg_match($usernameRegex, $username)) { // could probably make this the username
+        echo "<p>Sorry, the username you filled in contains one or more characters that are not accepted.</p>";
+      } else if (!preg_match($generalRegex, $password)) { // then this one's the password
+        echo "<p>Sorry, the password(s) you filled in contain one or more characters that are not accepted.</p>";
+      } else if (!preg_match($generalRegex, $paltform)) {
+        echo "<p>Sorry, the platform(s) you filled contain one or more characters that are not accepted.</p>";
+      } else {
+  
+        try {
+          // use exec() because no results are returned
+  
+          //!stmt moet nog worden aangepast om ook de filename in de database te zetten
+          $stmt = $this->conn->prepare("INSERT INTO users (username, password)
+          VALUES (:username, :password)"); // the 'users' here isn't an array, it's the table in the db.
+
+          $stmt->bindParam(':username', $username); // username
+          $stmt->bindParam(':password', $password); // password
+  
+          // $this->conn->exec($sql);
+          $stmt->execute();
+          echo " New record created successfully.";
+        } catch(PDOException $e) {
+          echo $sql . "<br>" . $e->getMessage();
+        }
+  
+      }
+    
+  }
+
+  public function select() {
+    //hier komt weer een try / catch
+    //bij try, komt SELECT * from games
+    //bij catch: error
+
+    $games = [];
+
+    try {
+    
+      $stmt = $this->conn->prepare("SELECT * FROM games");
+      $stmt->execute();
+    
+      // set the resulting array to associative
+      //Dit maakt van een resultset een Array (lijst)
+      $results = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      //dit zorgt ervoor dat $result gevult wordt met alle data in een array
+      $results = $stmt->fetchAll();
+
+      foreach($results as $result) {
+        $game = new Game($result['id'], $result['image'], $result['username'], $result['password'], $result['platform'], $result['release_year'], $result['rating'], $result['price']);
+        array_push($games, $game);
+      }
+
+      return $games;
+
+    } catch (PDOException $e) {
+      echo "Error: " . $e->getMessage();
     }
 
-    public function insert($data, $fileName) {
+  }
+  
+  public function getGame($id) {
 
-        $title = htmlspecialchars($data['title']); 
-        $genre = htmlspecialchars($data['genre']); 
-        $platform = htmlspecialchars($data['platform']);
-        $release_year = htmlspecialchars($data['release_year']);
-        $rating = htmlspecialchars($data['rating']);
-        $price = htmlspecialchars($data['price']);
+    try {
     
-        $generalRegex = '/(?!<>\/;\\[\\]{}`~)[A-Za-z0-9 ]*/';
-        // $usernameRegex = '/[A-Za-z0-9_]*/';
-        // $passwordRegex = '/placeholder/';
+      $stmt = $this->conn->prepare("SELECT * FROM games WHERE id = $id");
+      $stmt->execute();
     
-        if (!preg_match($generalRegex, $title)) { // could probably make this the username
-          echo "<p>Sorry, the title you filled in contains one or more characters that are not accepted.</p>";
-        } else if (!preg_match($generalRegex, $genre)) { // then this one's the password
-          echo "<p>Sorry, the genre(s) you filled in contain one or more characters that are not accepted.</p>";
-        } else if (!preg_match($generalRegex, $paltform)) {
-          echo "<p>Sorry, the platform(s) you filled contain one or more characters that are not accepted.</p>";
-        } else {
-    
-          try {
-            // use exec() because no results are returned
-    
-            //!stmt moet nog worden aangepast om ook de filename in de database te zetten
-            $stmt = $this->conn->prepare("INSERT INTO games (image, title, genre, platform, release_year, rating, price)
-            VALUES (:image, :title, :genre, :platform, :release_year, :rating, :price)");
-    
-            $stmt->bindParam(':image', $fileName);
-            $stmt->bindParam(':title', $title); // username
-            $stmt->bindParam(':genre', $genre); // password
-            $stmt->bindParam(':platform', $platform);
-            $stmt->bindParam(':release_year', $release_year);
-            $stmt->bindParam(':rating', $rating);
-            $stmt->bindParam(':price', $price);
-    
-            // $this->conn->exec($sql);
-            $stmt->execute();
-            echo " New record created successfully.";
-          } catch(PDOException $e) {
-            echo $sql . "<br>" . $e->getMessage();
-          }
-    
-        }
-      
-      }
-    
-      public function select() {
-        //hier komt weer een try / catch
-        //bij try, komt SELECT * from games
-        //bij catch: error
-    
-        $games = [];
-    
-        try {
-        
-          $stmt = $this->conn->prepare("SELECT * FROM games");
-          $stmt->execute();
-        
-          // set the resulting array to associative
-          //Dit maakt van een resultset een Array (lijst)
-          $results = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-          //dit zorgt ervoor dat $result gevult wordt met alle data in een array
-          $results = $stmt->fetchAll();
-    
-          foreach($results as $result) {
-            $game = new Game($result['id'], $result['image'], $result['title'], $result['genre'], $result['platform'], $result['release_year'], $result['rating'], $result['price']);
-            array_push($games, $game);
-          }
-    
-          return $games;
-    
-        } catch (PDOException $e) {
-          echo "Error: " . $e->getMessage();
-        }
-    
-      }
-    
-      public function fileUpload($file) {
-    
-        //target dir locatie waar het plaatje wordt neergezet
-        $target_dir = "uploads/";
-        $target_file = $target_dir . basename($file["name"]);
-        $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-    
-        // Check if image file is a actual image or fake image
-        $check = getimagesize($file["tmp_name"]);
-        if($check !== false) {
-          echo "File is an image - " . $check["mime"] . ".";
-          $uploadOk = 1;
-        } else {
-          echo "File is not an image.";
-          $uploadOk = 0;
-        }
-    
-        // Check if file already exists
-        if (file_exists($target_file)) {
-          echo "Sorry, file already exists.";
-          $uploadOk = 0;
-        }
-    
-        // Check file size
-        if ($file["size"] > 500000) {
-          echo "Sorry, your file is too large.";
-          $uploadOk = 0;
-        }
-    
-        // Allow certain file formats
-        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif" ) {
-          echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-          $uploadOk = 0;
-        }
-    
-        // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 0) {
-          echo "Sorry, your file was not uploaded.";
-          return false;
-        // if everything is ok, try to upload file
-        } else {
-          if (move_uploaded_file($file["tmp_name"], $target_file)) {
-            echo "The file ". htmlspecialchars( basename( $file["name"])). " has been uploaded.";
-            return true;
-          } else {
-            echo "Sorry, there was an error uploading your file.";
-            return false;
-          }
-        }
-    
-      }
-    
-      public function getGame($id) {
-    
-        try {
-        
-          $stmt = $this->conn->prepare("SELECT * FROM games WHERE id = $id");
-          $stmt->execute();
-        
-          // set the resulting array to associative
-          //Dit maakt van een resultset een Array (lijst)
-          $results = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-          //dit zorgt ervoor dat $result gevult wordt met alle data in een array
-          $results = $stmt->fetchAll();
-    
-          return $results;
-    
-        } catch (PDOException $e) {
-          echo "Error: " . $e->getMessage();
-        }
-    
-      }
-    
+      // set the resulting array to associative
+      //Dit maakt van een resultset een Array (lijst)
+      $results = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      //dit zorgt ervoor dat $result gevult wordt met alle data in een array
+      $results = $stmt->fetchAll();
+
+      return $results;
+
+    } catch (PDOException $e) {
+      echo "Error: " . $e->getMessage();
+    }
+
+  }
+  
 }
 
 ?>
--- app/oefenopdracht1/index.php
+++ app/oefenopdracht1/index.php
@@ -12,7 +12,7 @@ spl_autoload_register(function ($class) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Login</title>
+    <title>User Registration</title>
     <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
@@ -29,8 +29,21 @@ spl_autoload_register(function ($class) {
 
 <form action="Formhandling.php" method="post" enctype="multipart/form-data">
 
+<p>Please pick a username. You can use letters and numbers,<br>
+   but no special characters with the exception of underscores are allowed.
+</p>
+
+<br>
+
 <label for="username">Username:</label><br>
 <input type="text" class="textfield" name="username" required><br>
+
+<p>Please choose a password, and make sure to remember it and keep it safe.<br>
+   Please use a combination of letters, numbers, and special characters,<br>
+   and make sure it's at least 8 characters long.
+</p>
+
+<br>
 
 <label for="password">Password:</label><br>
 <input type="password" class="textfield" name="password" required><br>
